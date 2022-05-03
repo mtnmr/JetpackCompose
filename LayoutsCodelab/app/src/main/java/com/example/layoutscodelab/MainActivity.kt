28745fe19c30254a6f863d2671e9cebb739c5892
mtnmr <mei.rila1013@gmail.com>
2022-05-03 19:46:43 +0900

    list

--- LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
+++ LayoutsCodelab/app/src/main/java/com/example/layoutscodelab/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -14,6 +17,7 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
@@ -21,7 +25,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,7 +57,8 @@ fun LayoutCodelab() {
             )
         }
     ){ innerPadding ->
-        BodyContent(Modifier.padding(innerPadding))
+//        BodyContent(Modifier.padding(innerPadding))
+        LazyList()
     }
 }
 
@@ -63,6 +70,55 @@ fun BodyContent(modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun LazyList(){
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+    
+    Column() {
+        Row() {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+               Text(text = "Scroll to the end")
+            }
+        }
+        
+        
+        LazyColumn(state = scrollState){
+            items(100){
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index:Int){
+    Row(verticalAlignment = CenterVertically) {
+
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(modifier = Modifier.width(10.dp))
+        Text(text = "Item #$index", style = MaterialTheme.typography.subtitle1)
+    }    
+}
 
 @Preview
 @Composable

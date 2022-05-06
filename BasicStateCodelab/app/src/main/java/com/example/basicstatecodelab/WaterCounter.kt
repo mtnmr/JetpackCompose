035257d4fb8fab6d0449193582d1e7d157ea5f2b
mtnmr <mei.rila1013@gmail.com>
2022-05-06 15:01:30 +0900

    remember state

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -12,12 +12,16 @@ import androidx.compose.ui.unit.dp
 fun WaterCounter(modifier: Modifier = Modifier){
 
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+//        val count: MutableState<Int> =  remember {mutableStateOf(0)}
+        var count by remember { mutableStateOf(0)}
 
-        Text(text = "You've had $count glasses.")
+        if (count > 0){
+            Text(text = "You've had $count glasses.")
+        }
 
         Button(onClick = { count++ },
-            modifier = modifier.padding(top = 8.dp)
+            modifier = modifier.padding(top = 8.dp),
+            enabled = count < 10
         ) {
             Text(text = "Add One")
         }

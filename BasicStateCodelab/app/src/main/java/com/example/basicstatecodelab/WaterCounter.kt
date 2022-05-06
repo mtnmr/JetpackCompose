829b1935ad91968acf45eb1e4addd6b3552570c9
mtnmr <mei.rila1013@gmail.com>
2022-05-06 15:31:23 +0900

    remember in composition

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -1,6 +1,7 @@
 package com.example.basicstatecodelab
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
@@ -10,20 +11,41 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier){
-
     Column(modifier = modifier.padding(16.dp)) {
-//        val count: MutableState<Int> =  remember {mutableStateOf(0)}
-        var count by remember { mutableStateOf(0)}
-
-        if (count > 0){
-            Text(text = "You've had $count glasses.")
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
         }
-
-        Button(onClick = { count++ },
-            modifier = modifier.padding(top = 8.dp),
-            enabled = count < 10
-        ) {
-            Text(text = "Add One")
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
         }
     }
+
+    //compositionから離れるとstateがリセットされる例
+    //count=0のボタンを押してリコンポジションされた時、showStateは忘れられて、初期化される
+//    Column(modifier = modifier.padding(16.dp)) {
+//        var count by remember { mutableStateOf(0)}
+//
+//        if (count > 0){
+//            var showTask by remember { mutableStateOf(true)}
+//            if (showTask){
+//                WellnessTaskItem(
+//                    onClose = { showTask = false },
+//                    taskName = "Have you taken your 15 minute walk today?",
+//                    )
+//            }
+//
+//            Text(text = "You've had $count glasses.")
+//        }
+//
+//        Row(modifier = Modifier.padding(top = 8.dp)) {
+//            Button(onClick = { count++ }, enabled = count < 10) {
+//                Text(text = "Add One")
+//            }
+//            Button(onClick = { count = 0 }, modifier = Modifier.padding(start = 8.dp)) {
+//                Text(text = "Clear water count")
+//            }
+//
+//        }
+//    }
 }

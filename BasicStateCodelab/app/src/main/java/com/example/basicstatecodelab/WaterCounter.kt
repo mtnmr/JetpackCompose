2efe74939cbeb1f88facd94dc811641a935794f3
mtnmr <mei.rila1013@gmail.com>
2022-05-06 17:12:14 +0900

    state hoisting

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -6,20 +6,44 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
+
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
-    Column(modifier = modifier.padding(16.dp)) {
-        var count by remember { mutableStateOf(0) }
-        if (count > 0) {
-            Text("You've had $count glasses.")
-        }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
-            Text("Add one")
-        }
-    }
+fun StatelessCounter(count:Int, onIncrement:() -> Unit, modifier:Modifier = Modifier){
+  Column(modifier = modifier.padding(16.dp)) {
+      if (count > 0){
+          Text("You've had $count glasses.")
+      }
+      Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+          Text("Add one")
+      }
+  }  
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier){
+    var count by rememberSaveable { mutableStateOf(0) }
+    StatelessCounter(count = count, onIncrement = { count++ }, modifier)
+}
+
+
+
+
+//これを、値を保持して変更するstatefulと、それ以外のstatelessに分解する
+//@Composable
+//fun WaterCounter(modifier: Modifier = Modifier){
+//    Column(modifier = modifier.padding(16.dp)) {
+//        var count by rememberSaveable { mutableStateOf(0) }
+//        if (count > 0) {
+//            Text("You've had $count glasses.")
+//        }
+//        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
+//            Text("Add one")
+//        }
+//    }
 
     //compositionから離れるとstateがリセットされる例
     //count=0のボタンを押してリコンポジションされた時、showStateは忘れられて、初期化される
@@ -48,4 +72,3 @@ fun WaterCounter(modifier: Modifier = Modifier){
 //
 //        }
 //    }
-}

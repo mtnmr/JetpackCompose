6d3c6b3fe9fea30953156c1480921c7bc3397d89
mtnmr <mei.rila1013@gmail.com>
2022-07-12 18:00:40 +0900

    tip calculate (State, keyboard)

--- BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -12,24 +12,22 @@ import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun StatelessCounter(count:Int, onIncrement:() -> Unit, modifier:Modifier = Modifier){
-  Column(modifier = modifier.padding(16.dp)) {
-      if (count > 0){
-          Text("You've had $count glasses.")
-      }
-      Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
-          Text("Add one")
-      }
-  }  
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
 }
 
 @Composable
-fun StatefulCounter(modifier: Modifier = Modifier){
+fun StatefulCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable { mutableStateOf(0) }
     StatelessCounter(count = count, onIncrement = { count++ }, modifier)
 }
-
-
 
 
 //これを、値を保持して変更するstatefulと、それ以外のstatelessに分解する
@@ -45,8 +43,8 @@ fun StatefulCounter(modifier: Modifier = Modifier){
 //        }
 //    }
 
-    //compositionから離れるとstateがリセットされる例
-    //count=0のボタンを押してリコンポジションされた時、showStateは忘れられて、初期化される
+//compositionから離れるとstateがリセットされる例
+//count=0のボタンを押してリコンポジションされた時、showTaskは忘れられて、初期化される
 //    Column(modifier = modifier.padding(16.dp)) {
 //        var count by remember { mutableStateOf(0)}
 //

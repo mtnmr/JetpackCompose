d44f2eb17476db2e213cfa763a621f214bf842a6
mtnmr <mei.rila1013@gmail.com>
2022-10-17 17:30:25 +0900

    Basic Unit3-3 Practice: Heroes View

--- SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -3,13 +3,27 @@ package com.example.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ComposeCompilerApi
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.superheroes.model.Hero
+import com.example.superheroes.model.HeroesRepository
 import com.example.superheroes.ui.theme.SuperHeroesTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HeroesScreen()
                 }
             }
         }
@@ -30,14 +44,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun HeroesScreen(){
+    Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = stringResource(id = R.string.app_name),
+                    style = MaterialTheme.typography.h1,
+                )
+            }
+        }
+    ){
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroesList = heroes)
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun HeroesList(
+    heroesList: List<Hero>
+){
+    LazyColumn(
+    ){
+        items(heroesList){ hero: Hero ->
+            HeroItem(hero = hero)
+        }
+    }
+}
+
+@Composable
+fun HeroItem(hero:Hero){
+    Card(
+        elevation = 2.dp,
+        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .sizeIn(minHeight = 72.dp)
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = stringResource(id = hero.nameRes),
+                    style = MaterialTheme.typography.h3,
+                )
+
+                Text(
+                    text = stringResource(id = hero.descriptionRes),
+                    style = MaterialTheme.typography.body1
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(72.dp)
+                    .clip(RoundedCornerShape(8.dp))
+            ){
+                Image(
+                    painter = painterResource(id = hero.imageRes),
+                    contentDescription = null,
+                    alignment = Alignment.TopCenter,
+                    contentScale = ContentScale.FillWidth
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun HeroesListPreview(){
     SuperHeroesTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            HeroesList(
+                HeroesRepository.heroes
+            )
+        }
     }
 }

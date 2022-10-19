2431e3a3b49fd34a9d7e6d247fce2949985ffb9c
mtnmr <mei.rila1013@gmail.com>
2022-10-19 16:44:06 +0900

    add enum class

--- ComposeNavigation/app/src/main/java/com/example/composenavigation/NavigationScreen.kt
+++ ComposeNavigation/app/src/main/java/com/example/composenavigation/NavigationScreen.kt
@@ -14,55 +14,47 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavType
+import androidx.navigation.Navigation
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 
+
+enum class NavigationScreen() {
+    Home,
+    Next
+}
+
 @Composable
-fun NavigationScreen(){
+fun NavigationApp() {
     val navController = rememberNavController()
 
     val backStackEntry by navController.currentBackStackEntryAsState()
 
-    val navigationIcon: (@Composable () -> Unit)? =
-        if (backStackEntry?.destination?.route != "home") {
-            {
-                IconButton(onClick = {
-                    navController.popBackStack()
-                }) {
-                    Icon(
-                        imageVector = Icons.Outlined.ArrowBack,
-                        contentDescription = "Back"
-                    )
-                }
-            }
-        } else {
-            null
-        }
-
-
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = stringResource(id = R.string.app_name))},
-                navigationIcon = navigationIcon
+            MyAppBar(
+                canNavigateBack = backStackEntry?.destination?.route != NavigationScreen.Home.name,
+                onClick = { navController.popBackStack() }
             )
         }
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = "home",
+            startDestination = NavigationScreen.Home.name,
             modifier = Modifier.padding(innerPadding)
-        ){
-            composable("home"){
-                HomeScreen(navController = navController)
+        ) {
+            composable(NavigationScreen.Home.name) {
+                HomeScreen(
+                    onClick = { navController.navigate("${NavigationScreen.Next.name}/$it") }
+                )
             }
             composable(
-                "next/{text}",
-                arguments = listOf(navArgument("text"){ type = NavType.StringType })
-            ){ 
+                "${NavigationScreen.Next.name}/{text}",
+                arguments = listOf(navArgument("text") { type = NavType.StringType })
+            ) {
                 NextScreen(it.arguments?.getString("text").toString())
             }
         }
@@ -70,10 +62,34 @@ fun NavigationScreen(){
 }
 
 @Composable
+fun MyAppBar(
+    canNavigateBack: Boolean,
+    onClick: () -> Unit
+) {
+    val navigationIcon: (@Composable () -> Unit)? = {
+        if (canNavigateBack) {
+            IconButton(onClick = onClick) {
+                Icon(
+                    imageVector = Icons.Outlined.ArrowBack,
+                    contentDescription = "Back"
+                )
+            }
+        } else {
+            null
+        }
+    }
+
+    TopAppBar(
+        title = { Text(text = stringResource(id = R.string.app_name)) },
+        navigationIcon = navigationIcon
+    )
+}
+
+@Composable
 fun HomeScreen(
-    navController: NavController
-){
-    var name by remember { mutableStateOf("")}
+    onClick: (String) -> Unit
+) {
+    var name by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -84,22 +100,28 @@ fun HomeScreen(
         Text(text = "Welcome Home!")
 
         Spacer(modifier = Modifier.padding(16.dp))
-        
+
         OutlinedTextField(
             value = name,
-            onValueChange = {name = it},
-            label = { Text(text = "what your name?") }
+            onValueChange = { name = it },
+            label = {
+                Text(text = "Enter your name")
+            },
+            singleLine = true
         )
-        Button(onClick = { navController.navigate("next/$name") }) {
+        Button(
+            onClick = { onClick(name) }
+        ) {
             Text(text = "Next")
         }
     }
 }
 
 @Composable
-fun NextScreen(text:String){
+fun NextScreen(text: String) {
     Column(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
             .padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
